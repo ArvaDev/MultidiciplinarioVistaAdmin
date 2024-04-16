080d8bcc53cb1cc96b06c3163aff7d3cbcf591f9
ArvaDev <armandorovi1590@gmail.com>
2024-04-15 20:57:32 -0600

    Estadisticas

--- src/Service/Statics.js
+++ src/Service/Statics.js
@@ -7,13 +7,13 @@ const date = {
 
 const config = {
     headers: {
-        'x-access-token': `${window.localStorage.getItem('token')}`,
+        'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MWQ2YTVlMGYwMTQ4M2QyNmI4OTY1NiIsImlhdCI6MTcxMzIzNDkxMywiZXhwIjoxNzEzMjM1ODEzfQ.9sObXWonc3zixD8VtYnwa_aI0uOHqvrejv7cxjG9JhM',
         'Content-Type': 'application/json'
     }
 };
 
 const GetStatics = () => {
-    return axios.post(`http://localhost:4000/api/v1/reports/`, date, config)
+    return axios.post(`http://18.233.236.214/api/v1/reports/`, date, config)
         .then((response) => {
             return response.data;
         })
--- src/pages/Stadistic/Stadistic.jsx
+++ src/pages/Stadistic/Stadistic.jsx
@@ -1,53 +1,42 @@
-import './Stadistic.css'
+import './Stadistic.css';
 import React, { useState, useEffect, useRef } from 'react';
 import Chart from 'chart.js/auto';
-import { api } from './ApiE'
 import GetStatics from '../../Service/Statics';
-
 
 export default function Stadistic() {
     const chartRef = useRef();
-    const products = []
     const [data, setData] = useState([]);
-    const amount = []
-    api.map((o) => {
-        products.push(o.name)
-        amount.push(o.amount)
-    })
-
-
 
     useEffect(() => {
         const getStatics = async () => {
             try { 
                 const data = await GetStatics();
-                console.log(data);
-
+                setData(data);
             } catch (error) {
                 console.log(error);
             }
-        }
+        };
         getStatics();
     }, []);
-   
 
     useEffect(() => {
         const ctx = chartRef.current.getContext('2d');
         const myChart = new Chart(ctx, {
             type: 'pie',
             data: {
-                labels: products,
+                labels: data.map(o => o.name),
                 datasets: [{
-                    label: 'Productos ',
-                    data: amount
+                    label: 'Productos',
+                    data: data.map(o => o.amount),
                 }]
             },
         });
         return () => myChart.destroy();
-    }, []);
+    }, [data]);
+
     return (
         <div className='StadisticClass'>
-            <div className='Header'></div>
+            <div className='Header'>Tus productos</div>
             <div className='content'>
                 <div className='DataCont'>
                     <p>Productos vendidos</p>
@@ -55,6 +44,9 @@ export default function Stadistic() {
                 </div>
                 <div className='DataCont'>
                     <p>Ingresos</p>
+                    {data.map((o) => (
+                        <div className='info' key={o.name}><p>{o.name}</p><p>${o.total * o.amount}</p></div>
+                    ))}
                 </div>
             </div>
         </div>
