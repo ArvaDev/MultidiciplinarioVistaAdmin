8c61d5aae78349251dd945b416b59dd4108e8146
ArvaDev <armandorovi1590@gmail.com>
2024-04-17 22:02:51 -0600

    Notificaciones

--- src/pages/Notificationes/Notificaciones.jsx
+++ src/pages/Notificationes/Notificaciones.jsx
@@ -1,5 +1,5 @@
-import './Notificaciones.css'
-import Header from '../../components/UI/header/Header'
+import './Notificaciones.css';
+import Header from '../../components/UI/header/Header';
 import Notificacion from '../../components/UI/notificacion/Notificacion';
 import TitleLabel from '../../components/UI/titleLabel/TitleLabel';
 import { getOrders } from '../../Service/Orders';
@@ -7,34 +7,39 @@ import { useState, useEffect } from 'react';
 
 export default function Notificaciones() {
     const [orders, setOrders] = useState([]);
+
     useEffect(() => {
         const fetchOrders = async () => {
             try {
                 const ordersData = await getOrders();
                 console.log(ordersData);
                 setOrders(ordersData.orders);
-
             } catch (error) {
                 console.log(error);
             }
-        }
+        };
         fetchOrders();
     }, []);
+
     return (
         <div className='NotificacionesClass'>
             <Header />
             <TitleLabel title={`Notificaciones`} />
             <div className='ContainerNot'>
-                {orders.map((orders, index) => (
-                    <Notificacion
-                        key={orders._id} 
-                        user={orders.buyerData.name} 
-                        mail={orders.buyerData.email} 
-                        id={orders._id}
-                        status={orders.status}
-                        productos={orders.products}
-                    />
-                ))}
+                {orders.length > 0 ? (
+                    orders.map((order, index) => (
+                        <Notificacion
+                            key={order._id} 
+                            user={order.buyerData.name} 
+                            mail={order.buyerData.email} 
+                            id={order._id}
+                            status={order.status}
+                            productos={order.products}
+                        />
+                    ))
+                ) : (
+                    <p>No hay notificaciones disponibles</p>
+                )}
             </div>
         </div>
     );
